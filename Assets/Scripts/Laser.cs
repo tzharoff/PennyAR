02cbf94392a685bb19af891120cc8dfa6d411ea6
Tony Zharoff <tony.zharoff@gmail.com>
2021-12-11 06:10:15 -0800

    Adding Laser Functionality

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject goSplosion;
+    [SerializeField] float DistanceToCheck = 0.001f;
+    [SerializeField] float forwardSpeed = 0.01f;
+    [SerializeField] float sphereSize = 0.005f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward + new Vector3(0f, 0f, forwardSpeed * Time.deltaTime);
+        Ray fRay = new Ray(transform.position, transform.forward);
+        if(Physics.SphereCast(fRay, sphereSize, out RaycastHit hit,DistanceToCheck))
+        {
+            Instantiate(goSplosion, hit.point + hit.normal,Quaternion.LookRotation(hit.normal, Vector3.up));
+            Destroy(gameObject);
+        }
     }
 }
